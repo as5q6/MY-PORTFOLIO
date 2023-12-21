cb26b77d43a90ecbea0c4d2528314b41d9aa9185
as5q6 <as5q6@umsystem.edu>
2023-12-21 12:46:50 -0600

    dark mode added but not fully funtion on all the website

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+
+// Event listener for the toggle button
+document.getElementById('dark-mode-toggle').addEventListener('click', toggleDarkMode);
